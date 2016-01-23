a14c5966b1ad8621e44a93d8b577e71dc1a9f253
Armin Ronacher <armin.ronacher@active-4.com>
2016-01-23 10:17:33 -0800

    Drop namespaces

--- rigidsearch/htmlprocessor.py
+++ rigidsearch/htmlprocessor.py
@@ -1,3 +1,4 @@
+import re
 import html5lib
 import warnings
 from lxml.cssselect import CSSSelector
@@ -16,20 +17,8 @@ class ProcessingError(Exception):
     pass
 
 
-XHTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
-namespaces = {
-    'html': XHTML_NAMESPACE
-}
-
-
 def compile_selector(sel):
-    return CSSSelector(sel, namespaces=namespaces, translator='html')
-
-
-def qname(tag):
-    if '{' in tag:
-        return tag
-    return '{%s}%s' % (XHTML_NAMESPACE, tag)
+    return CSSSelector(sel, translator='html')
 
 
 tree_walker = html5lib.getTreeWalker('lxml')
@@ -37,7 +26,7 @@ tree_walker = html5lib.getTreeWalker('lxml')
 
 def make_processor_from_config(config):
     return Processor(
-        title_cleanup_func=config.get('PROCESSOR_TITLE_CLEANUP_FUNC'),
+        title_cleanup_regex=config.get('PROCESSOR_TITLE_CLEANUP_REGEX'),
         content_selectors=config.get('PROCESSOR_CONTENT_SELECTORS'),
         ignore=config.get('PROCESSOR_IGNORE'),
         ignored_tags=config.get('PROCESSOR_IGNORED_TAGS'),
@@ -46,24 +35,26 @@ def make_processor_from_config(config):
 
 class Processor(object):
 
-    def __init__(self, title_cleanup_func=None,
+    def __init__(self, title_cleanup_regex=None,
                  content_selectors=None,
                  ignore=None,
                  ignored_tags=None):
         self.content_selectors = [compile_selector(sel) for sel in
-                                  content_selectors or ('html|body',)]
-        self.title_cleanup_func = title_cleanup_func
+                                  content_selectors or ('body',)]
+        if title_cleanup_regex is not None:
+            title_cleanup_regex = re.compile(title_cleanup_regex)
+        self.title_cleanup_regex = title_cleanup_regex
         self.ignore = [compile_selector(sel) for sel
                        in ignore or ('.nocontent',)]
-        self.ignored_tags = [qname(x) for x in ignored_tags
-                             or ('script', 'noscript', 'style', 'head')]
+        self.ignored_tags = list(ignored_tags
+                                 or ('script', 'noscript', 'style', 'head'))
 
     def is_ignored(self, node):
         if node.tag in self.ignored_tags:
             return True
         for sel in self.ignore:
             xpath = sel.path.replace('descendant-or-self::', 'self::')
-            matches = node.xpath(xpath, namespaces=namespaces)
+            matches = node.xpath(xpath)
             if matches and matches[0] is node:
                 return True
         return False
@@ -71,16 +62,19 @@ class Processor(object):
     def process_document(self, document):
         if isinstance(document, basestring):
             document = StringIO(document)
-        doc = html5lib.parse(document, treebuilder='lxml')
+        doc = html5lib.parse(document, treebuilder='lxml',
+                             namespaceHTMLElements=False)
         return self.process_tree(doc)
 
     def process_title_tag(self, title):
         if title is None:
             return None
         text = title.text
-        if self.title_cleanup_func is not None:
-            text = self.title_cleanup_func(text)
-        return text
+        if self.title_cleanup_regex is not None:
+            match = self.title_cleanup_regex.search(text)
+            if match is not None:
+                text = match.group(1)
+        return unicode(text)
 
     def process_content_tag(self, body):
         if body is None:
@@ -107,11 +101,11 @@ class Processor(object):
         rv = {}
 
         root = tree.getroot()
-        head = root.find(qname('head'))
+        head = root.find('head')
         if head is None:
             raise ProcessingError('Document does not parse correctly.')
 
-        title = head.find(qname('title'))
+        title = head.find('title')
         rv['title'] = self.process_title_tag(title)
 
         buf = []
